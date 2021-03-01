581eef0e2342151c75c036f951fa4bb7503f4307
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2021-03-01 04:44:22 -0500

    making function not case sensitive

--- num_of_occurences.py
+++ num_of_occurences.py
@@ -2,20 +2,22 @@
 def num_of_occurences(string):
     occurences = {}
     for i in string:
+        lower_i = i.lower()
         try:
-            occurences[i] += 1
+            occurences[lower_i] += 1
         except:
-            occurences[i] = 1
+            occurences[lower_i] = 1
     return occurences
 
 # 0(n^2) runtime 
 def num_of_occurences_alt(string):
     occurences = {}
     for i in string:
-        if i in occurences:
-            occurences[i] += 1
+        lower_i = i.lower()
+        if lower_i in occurences:
+            occurences[lower_i] += 1
         else:
-            occurences[i] = 1
+            occurences[lower_i] = 1
     return occurences
 
 
@@ -23,7 +25,7 @@ if __name__ == '__main__':
     print("Testing Number of Occurences Problem: ")
     test1 = num_of_occurences("aaaabbbb")
     print("string = 'aaaabbbb', passed: {}".format(test1['a'] == 4 and test1['b'] == 4 and len(test1) == 2))
-    test2 = num_of_occurences("abccccc")
+    test2 = num_of_occurences("abCCccc")
     print("string = 'abccccc', passed: {}".format(test2['a'] == 1 and test2['b'] == 1 and test2['c'] == 5 and len(test2) == 3))
     test3 = num_of_occurences("abcdbb")
     print("string = \"abcdbb\", passed: {}".format((test3['a'] == 1 and test3['b'] == 3 and test3['c'] == 1 and test3['d'] == 1) and len(test3) == 4))
